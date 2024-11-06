13eda8a9fc17318dfdf115398215f505d1d56bff
Rajkumar Jangam <rajkumarJangam66@gmail.com>
2024-11-06 18:25:45 +0530

    test case resolved

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello from cicd using docker"', () => {
+      expect(appController.getHello()).toBe('Hello from cicd using docker');
     });
   });
 });
